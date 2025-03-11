dd2fddd76dd75d0fc732790b8bbad777e4a6d1c2
nmstom <tom@newmorningschool.com>
2025-03-11 17:30:49 +0000

    Will you ever commit this?

--- CSharp/Projects/Hello World/Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
+++ CSharp/Projects/Hello World/Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca9a36a4472e7984d64a35d03e3d549bdf846fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e0c7882a69c89ac80661116f523cd3a069f727")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
